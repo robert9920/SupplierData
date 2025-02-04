9615907c5bcd2ab472ca070763f56429777bad6d
robertp9920 <roberthumberto2008@hotmail.com>
2025-02-04 12:37:35 -0500

    Se modifico la funcion para obtener links de la segunda busqueda, ahora se obtienen 5 links en total

--- google_extract_data.py
+++ google_extract_data.py
@@ -4,11 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
-import random
 
 # Función para extraer todo el texto de las páginas webs 
 def extract_text(url):
@@ -58,7 +54,7 @@ def get_links_googleweb(provname):
 
     ##### Primera busqueda ###########
     ## Keyword: Nombre del proveedor
-    ## Se extraen 3 resultados de esta página
+    ## Se extraen 3 resultados de esta búsqueda
 
     # Se imprime el nombre del proveedor en la barra de búsqueda de Google Web
     prov_field = driver.find_element(By.XPATH, value="//textarea[@id='APjFqb']")
@@ -70,7 +66,7 @@ def get_links_googleweb(provname):
     # Se buscan las páginas web obtenidas
     search_results = driver.find_elements(By.CSS_SELECTOR, "div.g")
   
-    # Existen páginas que se van a ignorar
+    # Existen páginas que se van a ignorar en esta búsqueda
     webs_ignore = ["linkedin", "universidadperu","facebook", "indeed"]
     n_web = 0 # Número de páginas web que se van considerando
 
@@ -80,36 +76,62 @@ def get_links_googleweb(provname):
         title = title_element.text # Título del link obtenido
         link_element = result.find_element(By.CSS_SELECTOR, "a")
         link = link_element.get_attribute("href") # Link obtenido
-        for count, web  in enumerate(webs_ignore, start = 1): # Se verifica que los links obtenidos no pertenezcan a las webs a ignorar
-            if web in link:
-                break
-            elif count < len(webs_ignore): # Se deben evaluar todos las webs ignore
-                pass
-            else:
-                links_web.append(link) # Si no pertenecen, se agregan
-                n_web = n_web + 1
-                print(f"Title: {title}\nLink: {link}\n")
+        if link in links_web: # Se verifica si el link ya se encuentra en la lista de links
+            pass # Se ignora la evaluación del link
+        else: # Se evalua si no se encuentra agregado
+            for count, web  in enumerate(webs_ignore, start = 1): # Se verifica que los links obtenidos no pertenezcan a las webs a ignorar
+                if web in link:
+                    break
+                elif count < len(webs_ignore): # Se deben evaluar todos las webs ignore
+                    pass
+                else:
+                    links_web.append(link) # Si no pertenecen, se agregan
+                    n_web = n_web + 1
+                    print(f"Title: {title}\nLink: {link}\n")
 
         if n_web == 3: # Se consideran 3 páginas web para esta búsqueda
             break
 
-    print(links_web)
+    ##### Segunda busqueda ###########
+    ## Keyword: Nombre del proveedor + "contacto"
+    ## Se extraen 2 resultados de esta búsqueda
+    # Se imprime el nombre del proveedor más la palabra "contacto" en la barra de búsqueda de Google Web
 
-    # # Se crea una lista para almacenar los links encontrados
-    # news_link = list()
+    prov_field = driver.find_element(By.XPATH, value="//textarea[@id='APjFqb']")
+    prov_field.send_keys(" contacto")
+    prov_field.send_keys(Keys.RETURN)
 
-    # for new in news_found:
-    #     try:
-    #         # Se obtiene el link de la noticia
-    #         news_link.append(new.get_attribute("href")) 
-    #     except Exception as e:
-    #         print(f"Error al obtener el enlace original: {e}")
-    #         return None
+    time.sleep(5) # Tiempo de espera
 
-    # # Cerrar el navegador
-    # driver.quit()
+    # Se buscan las páginas web obtenidas
+    search_results = driver.find_elements(By.CSS_SELECTOR, "div.g")
+  
+    # Existen páginas que se van a ignorar en esta búsqueda
+    webs_ignore = ["linkedin", "universidadperu","facebook", "indeed"]
 
-    # print(news_link)
-    # return(news_link)
+    # Se itera sobre cada resultado obtenido
+    for result in search_results:
+        title_element = result.find_element(By.CSS_SELECTOR, "h3")
+        title = title_element.text # Título del link obtenido
+        link_element = result.find_element(By.CSS_SELECTOR, "a")
+        link = link_element.get_attribute("href") # Link obtenido
+        if link in links_web: # Se verifica si el link ya se encuentra en la lista de links
+            pass # se ignora la evaluación del link
+        else: # se evalua si no se encuentra agregado
+            for count, web  in enumerate(webs_ignore, start = 1): 
+                if web in link: # Se verifica que los links obtenidos no pertenezcan a las webs a ignorar
+                    break
+                elif count < len(webs_ignore): # Se deben evaluar todos las webs ignore
+                    pass
+                else:
+                    links_web.append(link) # Si no pertenecen, se agregan
+                    n_web = n_web + 1
+                    print(f"Title: {title}\nLink: {link}\n")
 
-get_links_googleweb("Lima Analytics")
+        if n_web == 5: # Se consideran 3 páginas web para esta búsqueda
+            break
+
+    return links_web
+
+
+get_links_googleweb("Schneider Electric Perú")
